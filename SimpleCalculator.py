7c9479ace546cde2cb64664464704593fbb1c541
Sudharshan <sudharshanhg2003@gmail.com>
2024-10-09 19:39:31 +0530

    As simple calculator

---
+++ SimpleCalculator.py
@@ -0,0 +1,23 @@
+print("simple calculator")
+print("select an operation")
+print("1.addition\n2.subtraction \n3.multiplication\n4.division\n5.modulus")
+while True:
+    operation=int(input("Enter an option>> "))
+    num1=float(input("enter num1>>"))
+    num2=float(input("enter a num2>>"))
+
+    if operation==1:
+        print(num1+num2)
+    elif operation==2:
+        print(num1-num2)
+    elif operation==3:
+        print(num1*num2)
+    elif operation==4:
+        print(num1/num2)
+    elif operation==5:
+        print(num1%num2)
+
+
+    else:
+        print("Invalid input.Try again")
+        break
